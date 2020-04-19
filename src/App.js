a5df39080e59d99c8082f582f287737b3a0c4bf6
dae-hyun-kim <daehk4@uci.edu>
2020-04-19 03:55:50 +0000

    components rendered to screen - Dae Kim

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import { Cards, Chart, CountryPicker} from './components'
+import styles from './App.module.css'
+
 class App extends React.Component {
   render() {
     return (
-      <div>
-        hello
+      <div className={styles.container}>
+        <Cards/>
+        <CountryPicker/>
+        <Chart/>
       </div>
     )
   }
