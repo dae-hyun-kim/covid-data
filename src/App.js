6b0fa076cbc9760ad2180db665673acab853ed48
dae-hyun-kim <daehk4@uci.edu>
2020-04-19 04:48:52 +0000

    reformatted styling - Dae Kim

--- src/App.js
+++ src/App.js
@@ -5,17 +5,27 @@ import styles from './App.module.css'
 import { fetchData } from './api'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+    }
+  }
+
 
   async componentDidMount() {
-    const data = await fetchData();
+    const fetchedData = await fetchData();
 
-    console.log(data)
+    this.setState({
+      data: fetchedData
+    })
   }
 
   render() {
+    const data = this.state.data
     return (
       <div className={styles.container}>
-        <Cards/>
+        <Cards data={data}/>
         <CountryPicker/>
         <Chart/>
       </div>
